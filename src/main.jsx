d5f4c3f35693a63c724f3ed6840c9425b2e15fdd
joacir <joacirprog@gmail.com>
2022-12-14 11:08:56 -0300

    rooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
+import { ThemeProvider } from "styled-components";
+
+import { AuthProvider } from "./hooks/auth";
 
 import theme from "./styles/theme";
 
@@ -13,7 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <AuthProvider >
         <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,48 @@
+import { useState } from 'react'
 import { Container, Form, Background } from './styles';
 import { FiUser ,FiMail, FiLock } from 'react-icons/fi';
-import { Link } from 'react-router-dom' 
+import { Link, useNavigate } from 'react-router-dom' 
+
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 
 export function SignUp(){
+     /* state, function */
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    //console.log(name, email, password);
+    if(!name || !email || !password) {
+      return alert('Prencha todos os campos!');
+    }
+
+    api.post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!');
+        navigate('/');
+      })
+      .catch((error) => {
+        if(error.response){
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      });
+  }
+
   return (
     <Container>
       <Background />
       
       <Form>
-        <h1>Rocket Notes</h1>
+        <h1>RocketNotes</h1>
         <p>Aplicações para salvar e gerenciar seus links úteis.</p>
 
         <h2>Crie sua conta</h2>
@@ -20,19 +50,25 @@ export function SignUp(){
         <Input 
           placeholder='Nome'
           type='text'
-          icon={FiUser}/>
+          icon={FiUser}
+          onChange = {event => setName(event.target.value)}
+          />
 
         <Input 
           placeholder='E-mail'
           type='text'
-          icon={FiMail}/>
+          icon={FiMail}
+          onChange = {event => setEmail(event.target.value)}
+          />
 
           <Input 
           placeholder='Senha'
           type='password'
-          icon={FiLock}/>
+          icon={FiLock}
+          onChange = {event => setPassword(event.target.value)}
+          />
 
-          <Button title='Cadastrar' />
+          <Button title='Cadastrar' onClick={handleSignUp} />
 
           <Link to="/">Voltar para o login</Link>
       </Form>
